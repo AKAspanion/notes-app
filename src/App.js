25ffcb91b7ea815cca419c2e4657e05fd6f1ce79
Ankit Kumar <panditankit1995@gmail.com>
2020-07-16 17:40:16 +0530

    🎉 Project created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import { MainPage } from "./pages";
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainPage() {
+  return <div className="main-page">This is main page</div>;
+}
+
+export default MainPage;
