4442951337f6492443cfbf27e61007a2995d7b65
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-01-29 14:59:11 +0530

    Updates

---
+++ precache-manifest.97d6608892dbd4040dc4280c2ab0cbcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581ef1a56312306aa24d98c5a9c314fa",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "2a1fe24f69ef64cdeec6",
+    "url": "/notes-app/static/css/2.5036aa7d.chunk.css"
+  },
+  {
+    "revision": "400556c66406bb2badbd",
+    "url": "/notes-app/static/css/main.7eac72aa.chunk.css"
+  },
+  {
+    "revision": "2a1fe24f69ef64cdeec6",
+    "url": "/notes-app/static/js/2.072105c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/notes-app/static/js/2.072105c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400556c66406bb2badbd",
+    "url": "/notes-app/static/js/main.c4f1161d.chunk.js"
+  },
+  {
+    "revision": "760c688f173c6bfa1fac",
+    "url": "/notes-app/static/js/runtime-main.482a3d78.js"
+  }
+]);
